a44e753ada2da1ec005d32825a2505276ab42067
yyyy <979731020@qq.com>
2022-02-25 14:45:59 +0800

    'eret'

--- js/index.js
+++ js/index.js
@@ -54,3 +54,37 @@ $(function () {
         }
     })
 })
+
+// 轮播图
+window.addEventListener('load', function () {
+    var li_ = document.getElementsByClassName('lis')
+    var all = document.getElementById('fat')
+    var index = 0
+    var time = null, time1 = null
+    function fn() {
+        time = setInterval(function () {
+            index++
+            if (index > 3) {
+                index = 0
+                all.scrollLeft = 0
+            }
+            move()
+        }, 1000)
+    }
+    fn()
+    function move() {
+        var step = 0
+        var stepmax = 20;
+        var star = all.scrollLeft //0
+        var end = li_[0].scrollWidth * 5 * index
+        var everystep = (end - star) / stepmax
+        time1 = setInterval(function () {
+            step++
+            if (step >= stepmax) {
+                clearInterval(time1)
+            }
+            star += everystep
+            all.scrollLeft = star
+        }, 15)
+    }
+})
