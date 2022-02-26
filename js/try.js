11f50dec2718a618b4c291a05b16eb7aa2e781ca
yyyy <979731020@qq.com>
2022-02-26 09:32:54 +0800

    'oooo'

--- js/try.js
+++ js/try.js
@@ -4,15 +4,20 @@ $(function () {
         $('.find_r>ul>li').removeClass('change')
         $('.find_r>ul>li').eq(index).addClass('change')
         $('.all').eq(index).css('display', 'block').siblings('.all').css('display', 'none')
+        if (index == 1 || index == 3) {
+            ajax("http://192.168.31.110:3000/useing/master", "mo", "out")
+        } else if (index == 0 || index == 2) {
+            ajax("http://192.168.31.110:3000/useing/public", "mo", "out")
+        }
     })
     $('.find_t>ul>li').click(function () {
         var index = $(this).index()
         $('.find_t>ul>li').eq(index).addClass('change').siblings().removeClass('change')
         $('.all').eq(index).css('display', 'block').siblings('.all').css('display', 'none')
-        // if (index == 0) {
-        //     $('out').eq(index).css('display', 'block').siblings('out').css('display', 'none')
-        // } else if (index == 1) {
-        //     $('outq').eq(index).css('display', 'block').siblings('outq').css('display', 'none')
-        // }
+        if (index == 1 || index == 3) {
+            ajax("http://192.168.31.110:3000/useing/master", "mo", "out")
+        } else if (index == 0 || index == 2) {
+            ajax("http://192.168.31.110:3000/useing/public", "mo", "out")
+        }
     })
 })
